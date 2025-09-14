b369fa3929a7ef4259d8c51a72a78cb3159a8275
bramsalaoui2712-ctrl <ton-email-github@example.com>
2025-09-14 21:21:28 +0200

    build: pin webpack chain (schema-utils@3, terser@5) + disable minimizers

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,8 +2,8 @@ module.exports = {
   webpack: {
     configure: (config) => {
       if (config.optimization) {
-        config.optimization.minimize = false;     // coupe la minification
-        config.optimization.minimizer = [];       // enlève Terser/CSS Minimizer
+        config.optimization.minimize = false;
+        config.optimization.minimizer = [];
       }
       return config;
     },
